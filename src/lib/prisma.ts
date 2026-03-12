25f206a1a5bebc662625c3cb72829b20fecff425
adamfadil17 <adam.fadilah17@gmail.com>
2026-03-12 11:04:19 +0700

    db url3

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,8 +4,7 @@ import { PrismaClient } from "prisma-client/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
-export const prisma = new PrismaClient({ adapter });
+export { prisma };
