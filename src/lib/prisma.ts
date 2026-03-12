62bae2a8b942d670cd9b3d24127b3e2674f37b5a
adamfadil17 <adam.fadilah17@gmail.com>
2026-03-12 09:59:34 +0700

    ubah settingan prisma ke helmy

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,7 @@
+import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@/generated/prisma";
+import { PrismaClient } from "prisma-client/client";
+
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
