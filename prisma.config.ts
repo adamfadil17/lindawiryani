4b7378b760858ce3fdbd7ef05e08cb0ed0fb334a
adamfadil17 <adam.fadilah17@gmail.com>
2026-03-12 11:13:46 +0700

    db url6

--- prisma.config.ts
+++ prisma.config.ts
@@ -10,6 +10,6 @@ export default defineConfig({
     seed: "tsx prisma/seed.ts",
   },
   datasource: {
-    url: process.env["DATABASE_URL"] as string,
+    url: process.env.DATABASE_URL ?? "",
   },
 });
