2cd791f664fd19d7839766d8fc9870a02070aafe
adamfadil17 <adam.fadilah17@gmail.com>
2026-04-28 20:38:23 +0700

    update next config allow all 2

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,12 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "**",
+      },
       {
         protocol: "https",
         hostname: "**",
